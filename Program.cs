3d2a6a717ba953a016272e015a0e9d6adda155a4
Alejandro P <alejandroparparcen@yahoo.com>
2019-04-13 21:55:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.UtcNow.Date;
+            DateTime today = DateTime.Today;
+            Console.WriteLine(dateTime.ToString("dd/MM/yyyy"));
+            Console.WriteLine(dateTime.ToString("d"));
+            Console.WriteLine(today.ToString("dd-MM-yyyy"));
+            Console.ReadLine();
+
+        }
+    }
+}
